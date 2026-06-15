7a6acfa1d4ea8db0f1c797e2517997fd76e99337
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2026-06-15 19:36:49 +0700

    fix home customer

--- app/dashboard/customer/page.tsx
+++ app/dashboard/customer/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-import { SnapHeader } from "@/components/layout/SnapHeader";
+import { SnapHeader } from "@/components/snap/SnapCommon";
 
 import {
   FaArrowRight,
